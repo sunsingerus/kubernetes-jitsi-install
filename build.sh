0a736232dae7659eaa3932f4a513112439b48e86
Vladislav Klimenko <sunsingerus@gmail.com>
2019-10-19 00:55:25 +0300

    typo

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-cat web-template.yaml | (set -a; source env.example; set _a; envsubst) > web-generated.yaml
-cat jvb-template.yaml | (set -a; source env.example; set _a; envsubst) > jvb-generated.yaml
-cat jicofo-template.yaml | (set -a; source env.example; set _a; envsubst) > jicofo-generated.yaml
-cat prosody-template.yaml | (set -a; source env.example; set _a; envsubst) > prosody-generated.yaml
+cat web-template.yaml | (set -a; source env.example; set +a; envsubst) > web-generated.yaml
+cat jvb-template.yaml | (set -a; source env.example; set +a; envsubst) > jvb-generated.yaml
+cat jicofo-template.yaml | (set -a; source env.example; set +a; envsubst) > jicofo-generated.yaml
+cat prosody-template.yaml | (set -a; source env.example; set +a; envsubst) > prosody-generated.yaml
 
